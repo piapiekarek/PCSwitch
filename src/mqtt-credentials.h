c42b388bd3e0c6cb07793041c70fd12dabeb66db
Pia Piekarek <piapiekarek@googlemail.com>
2022-04-08 23:55:09 +0200

    add mqtt-credentials

---
+++ src/mqtt-credentials.h
@@ -0,0 +1,11 @@
+
+#ifndef MQTT_CREDENTIALS_H
+#define MQTT_CREDENTIALS_H
+
+#include <PubSubClient.h>
+
+const char* topicName = "topicName"; 
+const char* topicMsgString = "topicMsgString"; 
+IPAddress ipOfMqttBroker(192, 168, 178, 1);
+
+#endif
